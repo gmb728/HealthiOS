7d1e6f3a2eacc5bb0deece0580cf1c84930bff66
Chang Sophia <you@example.com>
2018-11-08 13:51:44 +0800

    Initial Commit

---
+++ HealthiOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HealthiOS
+//
+//  Created by Chang Sophia on 11/8/18.
+//  Copyright © 2018 Chang Sophia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
